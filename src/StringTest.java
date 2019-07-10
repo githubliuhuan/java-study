57803183e84acc98cbc185c7ad6649d472e7bfca
liuhuan <13942061310@163.com>
2019-07-10 23:39:02 +0800

    String

---
+++ src/StringTest.java
@@ -0,0 +1,10 @@
+public class StringTest {
+    public static void main(String[] args) {
+        /**
+         * 情景一：
+         */
+        String s1 = "abc";
+        String s2 = "abc";
+        System.out.println("s1 == s2 : "+(s1==s2));
+    }
+}
