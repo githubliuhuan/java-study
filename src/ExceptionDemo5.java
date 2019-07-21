1ded00b80c11cfc337967f43aef35dfbb6e970a4
liuhuan <13942061310@163.com>
2019-07-21 22:07:41 +0800

    first

---
+++ src/ExceptionDemo5.java
@@ -0,0 +1,31 @@
+class FuShuException extends Exception {
+    FuShuException(String msg) {
+        super(msg);
+    }
+}
+
+class Demo {
+    int div(int a, int b) throws FuShuException {
+        if (b<0)
+            throw new FuShuException("除数为负数");
+        return a/b;
+    }
+}
+
+public class ExceptionDemo5 {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        try {
+            int x = d.div(4, -1);
+            System.out.println("x="+x);
+        }
+        catch (FuShuException e) {
+            System.out.println(e.toString());
+            return;
+        }
+        finally {
+            System.out.println("finally");
+        }
+        System.out.println("over");
+    }
+}
