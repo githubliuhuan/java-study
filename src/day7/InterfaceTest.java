a80c29d6f425c2fbe71ff82670bd792ae67d0b48
liuhuan <13942061310@163.com>
2019-07-22 22:41:53 +0800

    first

---
+++ src/day7/InterfaceTest.java
@@ -0,0 +1,59 @@
+package day7;
+/*
+继承：is a
+接口：like a
+基本功能定义在类中
+扩展功能定义在接口中
+ */
+
+/*
+abstract class Student
+{
+    abstract void study();
+    void sleep()
+    {
+        System.out.println("sleep");
+    }
+}
+
+interface Smoking
+{
+    void smoke();
+}
+
+class ZhangSan extends Student implements Smoking
+{
+    void study(){};
+    public void smoke(){};
+}
+
+class Lisi extends Student
+{
+    void study(){};
+}
+*/
+
+/*
+abstract class Sporter
+{
+    abstract void play();
+
+}
+
+interface Study
+{
+
+}
+
+class Wangwu extends Sporter
+{
+    void play(){};
+
+}
+*/
+
+public class InterfaceTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
