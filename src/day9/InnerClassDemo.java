2be851eb0a38d74844141364e4574023ab06d920
huanliu78 <huanliu78@creditease.cn>
2019-07-25 15:30:50 +0800

    commit

--- src/day9/InnerClassDemo.java
+++ src/day9/InnerClassDemo.java
@@ -5,20 +5,37 @@ package day9;
     之所以可以直接访问外部类中的成员，是因为内部类持有了一个外部类的引用。格式：外部类名.this
 2.外部类要访问内部类，必须建立内部类对象。
 
+访问格式：
+当内部类定义在外部类的成员位置上，而且非私有，可以在外部其他类中
+可以直接建立内部类对象。
+格式：
+外部类名.内部类名 变量名 = 外部类对象.内部类对象
+Outer.Inner in = new Outer().new Inner()
+
+2.当内部类在成员位置上，就可以被成员修饰符修饰。
+    比如：private 将内部类在外部类中进行封装。
+    static 内部类就具备static的特性。
+    当内部类被static修饰后，只能直接访问外部类中的static成员，出现了访问局限。
+
+    在外部其他类中，如何直接访问静态内部类的静态成员呢？
+
+注意：当内部类中定义了静态成员，该内部类必须是static的。
+     当外部类中的静态方法访问内部类时，内部类也必须是static的。
  */
 
 
 class Outer
 {
-    private int x = 3;
+    private static int x = 3;
 
-    class Inner // 内部类,可以被private修饰
+    static class Inner // 内部类,可以被private修饰
     {
         int x = 4;
-        void function()
+        static void function()
         {
             int x = 6;
-            System.out.println("inner:"+Outer.this.x);
+            System.out.println("inner:"+x);
+            // System.out.println("inner:"+Outer.this.x);
         }
 
     }
@@ -30,15 +47,36 @@ class Outer
 //        in.function();
     }
 
+    static class Inner2
+    {
+        void show()
+        {
+            System.out.println("inner2 show");
+        }
+    }
+
+    public static void method2()
+    {
+        new Inner2().show();
+    }
+
 }
+
+
+
+
+
 
 
 public class InnerClassDemo {
     public static void main(String[] args) {
         Outer out = new Outer();
         out.method();
-        Outer.Inner in = new Outer().new Inner();
-        in.function();
+        // Outer.Inner in = new Outer().new Inner();
+        // in.function();
+
+         // new Outer.Inner().function();
+        Outer.Inner.function();
     }
 }
 
