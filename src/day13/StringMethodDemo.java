e0d170587d7ada3de215931e3e1689865a569aef
huanliu78 <huanliu78@creditease.cn>
2019-07-25 18:39:50 +0800

    commit

---
+++ src/day13/StringMethodDemo.java
@@ -0,0 +1,17 @@
+package day13;
+/*
+String类适用于描述字符串事物
+那么它就提供了多个方法对字符串进行操作
+
+常见的操作？
+1.length() 获取长度
+2.
+
+ */
+
+
+public class StringMethodDemo {
+    public static void main(String[] args) {
+
+    }
+}
